e4c7898725089bd011270a929aff9fedd120af36
Jacob <jacobcons@gmail.com>
2024-07-29 20:25:57 +0000

    beginnings of hashmap

--- hashmap/hashmap.go
+++ hashmap/hashmap.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Pair struct {
 	Key   string
@@ -11,37 +14,60 @@ func (p *Pair) String() string {
 	return fmt.Sprintf("(%v:%v)", p.Key, p.Value)
 }
 
-type LinkedList struct {
-	Value *Pair
-	Next  *LinkedList
+type linkedList struct {
+	value *Pair
+	next  *linkedList
 }
 
-func NewLinkedList(value *Pair) *LinkedList {
-	return &LinkedList{value, nil}
-}
-
-func (l *LinkedList) AddFirst(value *Pair) {
-	clone := &LinkedList{l.Value, l.Next}
-	l.Value = value
-	l.Next = clone
-}
-
-func (l *LinkedList) String() string {
-	if l.Next == nil {
-		return l.Value.String()
+func (l *linkedList) String() string {
+	if l.next == nil {
+		return l.value.String()
 	}
-	return fmt.Sprintf("%v->%v", l.Value, l.Next.String())
+	return fmt.Sprintf("%v->%v", l.value, l.next.String())
 }
 
 type HashMap struct {
-	Buckets []*LinkedList
+	buckets  []*linkedList
+	capacity uint32
+}
+
+func NewHashMap() *HashMap {
+	const INITIAL_CAPACITY = 4
+	return &HashMap{make([]*linkedList, INITIAL_CAPACITY), INITIAL_CAPACITY}
+}
+
+func (h *HashMap) String() string {
+	var sb strings.Builder
+	for i := uint32(0); i < h.capacity; i++ {
+		sb.WriteString(fmt.Sprintf("%v: %v\n", i, h.buckets[i]))
+	}
+	return sb.String()
+}
+
+func (h *HashMap) Put(key string, value interface{}) {
+	index := generateHashCode(key) % h.capacity
+	current := h.buckets[index]
+	if current == nil {
+		h.buckets[index] = &linkedList{&Pair{key, value}, nil}
+	}
+}
+
+// convert each character to ascii and write as base 31
+func generateHashCode(key string) uint32 {
+	var hash uint32 = 0
+	for i := 0; i < len(key); i++ {
+		hash = 31*hash + uint32(key[i])
+	}
+	return hash
 }
 
 func main() {
-	l := NewLinkedList(&Pair{"james", 3})
-	fmt.Println(l)
-	l.AddFirst(&Pair{"bob", 6})
-	fmt.Println(l)
-	l.AddFirst(&Pair{"leo", 1})
-	fmt.Println(l)
+	h := NewHashMap()
+	fmt.Println(h)
+
+	h.Put("bob", 3)
+	fmt.Println(h)
+
+	h.Put("james", 2)
+	fmt.Println(h)
 }
