cd5d6dc39fe789cf383c2af637bd453ecd2b2ced
Jacob Cons <jacobcons@gmail.com>
2025-01-13 12:16:59 +0000

    finished DFS+testing, setup BFS and tests

--- graph/adjacency-list/graph.go
+++ graph/adjacency-list/graph.go
@@ -13,6 +13,58 @@ func (g *Graph) AddEdge(v int, w int) {
 	g.List[w] = append(g.List[w], v)
 }
 
-func (g *Graph) Adj(v int) []int {
-	return g.List[v]
+func (g *Graph) DFS(v int) *PathData {
+	pathData := NewPathData(v)
+	var dfsHelper func(v int)
+	dfsHelper = func(v int) {
+		pathData.Visited[v] = true
+		for _, w := range g.List[v] {
+			_, hasVisited := pathData.Visited[w]
+			if hasVisited == false {
+				pathData.EdgeTo[w] = v
+				dfsHelper(w)
+			}
+		}
+	}
+	dfsHelper(v)
+	return pathData
+
+}
+
+func (g *Graph) BFS(v int) *PathData {
+	pathData := NewPathData(v)
+	return pathData
+
+}
+
+type PathData struct {
+	Source  int
+	Visited map[int]bool
+	EdgeTo  map[int]int
+}
+
+func NewPathData(v int) *PathData {
+	return &PathData{v, make(map[int]bool), make(map[int]int)}
+}
+
+func HasPathTo(pathData *PathData, v int) bool {
+	return pathData.Visited[v]
+}
+
+func PathTo(pathData *PathData, v int) []int {
+	if HasPathTo(pathData, v) == false {
+		return nil
+	}
+
+	path := []int{}
+	for v != pathData.Source {
+		path = prepend(path, v)
+		v = pathData.EdgeTo[v]
+	}
+	path = prepend(path, pathData.Source)
+	return path
+}
+
+func prepend[T any](arr []T, item T) []T {
+	return append([]T{item}, arr...)
 }
--- graph/adjacency-list/graph_test.go
+++ graph/adjacency-list/graph_test.go
@@ -10,13 +10,77 @@ func TestGraphApi(t *testing.T) {
 	g.AddEdge(0, 1)
 	g.AddEdge(0, 2)
 	g.AddEdge(4, 5)
-	if slices.Equal(g.Adj(0), []int{1, 2}) == false {
-		t.Errorf("expected g.Adj(0)=[1,2]")
+	if slices.Equal(g.List[0], []int{1, 2}) == false {
+		t.Errorf("expected g.List[0]=[1,2]")
 	}
-	if slices.Equal(g.Adj(4), []int{5}) == false {
-		t.Errorf("expected g.Adj(4)=[5]")
+	if slices.Equal(g.List[4], []int{5}) == false {
+		t.Errorf("expected g.List[4]=[5]")
 	}
-	if slices.Equal(g.Adj(3), []int{}) == false {
-		t.Errorf("expected g.Adj(3)=nil")
+	if slices.Equal(g.List[3], []int{}) == false {
+		t.Errorf("expected g.List[3]=[]")
+	}
+}
+
+func TestDFS(t *testing.T) {
+	g := createPopulatedGraph()
+	pathData := g.DFS(0)
+
+	testHasPathTo(pathData, t)
+
+	expectedPath := []int{0, 1, 2, 5, 4, 3}
+	testPathTo(pathData, expectedPath, t)
+}
+
+func TestBFS(t *testing.T) {
+	g := createPopulatedGraph()
+	pathData := g.BFS(0)
+
+	testHasPathTo(pathData, t)
+
+	expectedPath := []int{0, 1, 4, 3}
+	testPathTo(pathData, expectedPath, t)
+}
+
+func createPopulatedGraph() *Graph {
+	g := NewGraph()
+	g.AddEdge(0, 1)
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 4)
+	g.AddEdge(2, 5)
+	g.AddEdge(3, 4)
+	g.AddEdge(4, 5)
+	g.AddEdge(5, 6)
+	g.AddEdge(5, 8)
+	g.AddEdge(6, 7)
+	g.AddEdge(9, 10)
+	g.AddEdge(10, 11)
+	return g
+}
+
+func testHasPathTo(pathData *PathData, t *testing.T) {
+	connectedVertices := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	for _, v := range connectedVertices {
+		if HasPathTo(pathData, v) == false {
+			t.Errorf("HasPathTo should detect path from 0 to %v", v)
+		}
+	}
+
+	disconnectedVertices := []int{9, 10, 11, 12}
+	for _, v := range disconnectedVertices {
+		if HasPathTo(pathData, v) == true {
+			t.Errorf("HasPathTo shouldn't detect path from 0 to %v", v)
+		}
+	}
+}
+
+func testPathTo(pathData *PathData, expectedPath []int, t *testing.T) {
+	actualPath := PathTo(pathData, 3)
+	if slices.Equal(actualPath, expectedPath) == false {
+		t.Errorf("PathTo(0, 3)=%v, expected=%v", actualPath, expectedPath)
+	}
+
+	path := PathTo(pathData, 9)
+	if path != nil {
+		t.Errorf("PathTo(0, 9)=%v, expected=nil", path)
 	}
 }
